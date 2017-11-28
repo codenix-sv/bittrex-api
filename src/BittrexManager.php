373222ba97afe230fc44e61fec529355d0eeafd0
Codenix <codenix.sv@gmail.com>
2017-11-28 20:13:31 +0200

    Add copyright

--- src/BittrexManager.php
+++ src/BittrexManager.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link https://github.com/codenix-sv/bittrex-api
+ * @copyright Copyright (c) 2017 codenix-sv
+ * @license https://github.com/codenix-sv/bittrex-api/blob/master/LICENSE
+ */
 
 namespace codenixsv\Bittrex;
 
